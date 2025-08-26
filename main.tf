e647d33057403c68b87849f3ec0d96db5fb0a7d6
mehul911devops <mehul911@gmail.com>
2025-08-26 23:02:36 +0530

    added comment

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "mehulrg"
   location = "West Europe"
 }
-
+#hello mehul
 resource "azurerm_storage_account" "mehulstg" {
   name                     = "mehulstg24082025"
   resource_group_name      = azurerm_resource_group.example.name
